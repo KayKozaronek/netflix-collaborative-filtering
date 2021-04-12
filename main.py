304f5c1c811dfe5762057cc059f7a1137afd88f7
KayKozaronek <kozaronek@gmail.com>
2021-04-12 09:49:03 +0200

    running "kmeans" on dataset

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import common
 import naive_em
 import em
 
-X = np.loadtxt("toy_data.txt")
+X = np.loadtxt("data/toy_data.txt")
 
 # TODO: Your code here
+for k in range(1,5):
+    for seed in range(0,5):
+        m, p = common.init(X, k, seed)
+        m, p, c = kmeans.run(X, m, p)
+        common.plot(X, m, p, f"Plot + {k}")
+        print(c)
