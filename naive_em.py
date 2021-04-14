ab507a5718ce8cc8bd4891263801241f83afb1cf
KayKozaronek <kozaronek@gmail.com>
2021-04-14 13:44:02 +0200

    Implemented M step

--- naive_em.py
+++ naive_em.py
@@ -17,8 +17,10 @@ def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
             for all components for all examples
         float: log-likelihood of the assignment
     """
-    raise NotImplementedError
+    mu, var, p = mixture
+    log_likelihood = 
 
+    return soft_counts, log_likelihood
 
 def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
     """M-step: Updates the gaussian mixture by maximizing the log-likelihood
@@ -32,7 +34,20 @@ def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
     Returns:
         GaussianMixture: the new gaussian mixture
     """
-    raise NotImplementedError
+    n, K = post.shape
+    n, d = X.shape
+    
+    post_sum = np.sum(post, axis=0)
+    
+    mu_hat = np.dot(post.T, X) / post_sum.reshape(K,1)
+    p_hat = 1/n * post_sum
+    
+    norm = [np.sum(post[j] * np.linalg.norm(X - mu_hat[j])**2) for j in range(K)]
+    var_hat = norm / (d* post_sum) 
+    
+    gaussian_mixture = GaussianMixture(mu_hat, var_hat, p_hat)
+
+    return gaussian_mixture
 
 
 def run(X: np.ndarray, mixture: GaussianMixture,
@@ -51,3 +66,9 @@ def run(X: np.ndarray, mixture: GaussianMixture,
         float: log-likelihood of the current assignment
     """
     raise NotImplementedError
+
+    # convergence
+    while not new_ll - old_ll <= 10**(-6) * np.abs(new_ll):
+
+
+    return (gaussian_mixture, post, LL)
